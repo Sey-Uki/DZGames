e019085c0b67c1945b76903916f6004a90c2cb5c
Mavludin <imevlud@gmail.com>
2023-04-01 00:08:34 +0300

    feat: add carousel

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Button = ({width, text}: {width: string, text: string}) => {
-  const ButtonStyle = styled.button({
-    background: "linear-gradient(270deg, #FFA000 0%, #FF6F00 100%)",
-    boxShadow: "0px 5px 20px rgba(255, 159, 1, 0.36)",
-    borderRadius: "20px",
-    border: "none",
-    maxWidth: width,
-    width: "100%",
-    height: "50px",
-    color: "#ffffff",
-    display: "flex",
-    alignItems: "center",
-    fontSize: "18px",
-    justifyContent: "center",
-  });
-  return <ButtonStyle>{text}</ButtonStyle>;
-};
+export const Button = ({ width, text }: { width: string; text: string }) =>
+  <StyledButton width={width}>{text}</StyledButton>;
 
-
+const StyledButton = styled.button<{ width: string}>`
+  background: linear-gradient(270deg, #FFA000 0%, #FF6F00 100%);
+  box-shadow: 0px 5px 20px rgba(255, 159, 1, 0.36);
+  border-radius: 20px;
+  border: none;
+  max-width: ${({ width }) => width};
+  width: 100%;
+  height: 50px;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  justify-content: center;
+`;
